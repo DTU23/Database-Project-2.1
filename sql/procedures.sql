1f4a076538f6e138573ecbbb671f426dc067fb3b
Viktor Poulsen <viktormaigaard@gmail.com>
2017-03-28 09:05:51 +0200

    Fixed procedure

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -2,7 +2,8 @@ DELIMITER //
 CREATE PROCEDURE batch_details_from_id
 (IN input INT)
 BEGIN
-  SELECT produktbatch.pb_id, produktbatch.status, produktbatch.recept_id, recept.recept_navn FROM produktbatch, recept
-  WHERE produktbatch.pb_id = input;
+  SELECT produktbatchkomponent.rb_id, raavare_navn, leverandoer, netto, opr_id
+  FROM produktbatchkomponent NATURAL JOIN  raavare NATURAL JOIN  raavarebatch
+  WHERE produktbatchkomponent.pb_id = input;
 END //
 DELIMITER ;
