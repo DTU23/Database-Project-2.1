868900e2fdff8b967d1df201e313e60409c76715
Viktor Poulsen <viktormaigaard@gmail.com>
2017-03-28 11:45:25 +0200

    Removed trigger
    will be implemented differently from available data

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -4,7 +4,6 @@ CREATE TRIGGER after_component_insert
   AFTER INSERT ON productbatchcomponent
   FOR EACH ROW
 BEGIN
-  UPDATE producebatch SET amount = amount-new.netto WHERE producebatch.rb_id = new.rb_id;
   UPDATE productbatch SET status = 1 WHERE productbatch.pb_id = new.pb_id;
 END //
 DELIMITER ;
