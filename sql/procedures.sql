934272f4e2118044407cdbbca21fe2a4b9bfc9e9
Viktor Poulsen <viktormaigaard@gmail.com>
2017-03-28 10:57:30 +0200

    created procedures

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -1,5 +1,18 @@
+DROP PROCEDURE IF EXISTS get_batch_details_from_id;
+DROP PROCEDURE IF EXISTS create_product_batch_from_recept_id;
+DROP PROCEDURE IF EXISTS create_produce_batch_from_produce_id;
+DROP PROCEDURE IF EXISTS create_prescription;
+DROP PROCEDURE IF EXISTS create_produce;
+DROP PROCEDURE IF EXISTS delete_produce_batch_from_produce_id;
+DROP PROCEDURE IF EXISTS create_prescription_component;
+DROP PROCEDURE IF EXISTS create_operator;
+DROP PROCEDURE IF EXISTS update_operator;
+DROP PROCEDURE IF EXISTS get_operator_name_from_id;
+/**
+
+ */
 DELIMITER //
-CREATE PROCEDURE batch_details_from_id
+CREATE PROCEDURE get_batch_details_from_id
 (IN input INT)
 BEGIN
   SELECT produktbatchkomponent.rb_id, raavare_navn, leverandoer, netto, opr_id
@@ -8,10 +21,108 @@ BEGIN
 END //
 DELIMITER ;
 
+/**
+Foreman
+ */
 DELIMITER //
-CREATE PROCEDURE new_product_batch
+CREATE PROCEDURE create_product_batch_from_recept_id
 (IN recept_id_input INT)
 BEGIN
   INSERT INTO produktbatch(status, recept_id) VALUES(0, recept_id_input);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE create_produce_batch_from_produce_id
+(IN input_maengde INT, IN input_rb_id INT)
+BEGIN
+  INSERT INTO raavarebatch(maengde, raavare_id) VALUES(input_maengde, input_rb_id);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE delete_produce_batch_from_produce_id
+(IN input_rb_id INT)
+BEGIN
+  DELETE FROM raavarebatch WHERE raavarebatch.raavare_id = input_rb_id;
+END //
+DELIMITER ;
+
+/**
+Pharmacist
+ */
+DELIMITER //
+CREATE PROCEDURE create_prescription
+(IN input_recept_navn INT)
+BEGIN
+  INSERT INTO recept(recept_navn) VALUES(input_recept_navn);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE create_produce
+(IN input_produce_name INT, IN input_produce_leverandoer INT)
+BEGIN
+  INSERT INTO raavare(raavare_navn, leverandoer) VALUES(input_produce_name, input_produce_leverandoer);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE create_prescription_component
+(
+  IN input_prescription_id INT,
+  IN input_produce_id INT,
+  IN input_netto INT,
+  IN input_tolerance INT
+)
+BEGIN
+  INSERT INTO receptkomponent(recept_id, raavare_id, nom_netto, tolerance)
+  VALUES(input_prescription_id, input_produce_id, input_netto, input_tolerance);
+END //
+DELIMITER ;
+
+/**
+Admin
+*/
+DELIMITER //
+CREATE PROCEDURE create_operator
+(
+  IN input_id INT,
+  IN input_name INT,
+  IN input_initials INT,
+  IN input_cpr INT,
+  IN input_password INT
+)
+BEGIN
+  INSERT INTO operatoer(opr_id, opr_navn, ini, cpr, password) VALUES(input_id, input_name, input_initials, input_cpr, input_password);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE update_operator
+(
+  IN input_id INT,
+  IN input_name INT,
+  IN input_initials INT,
+  IN input_cpr INT,
+  IN input_password INT
+)
+BEGIN
+  UPDATE operatoer SET
+    opr_navn=input_name,
+    ini = input_initials,
+    cpr = input_cpr,
+    password = input_password
+  WHERE input_id = opr_id;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE get_operator_name_from_id
+(
+  IN input_id INT
+)
+BEGIN
+  SELECT opr_navn FROM operatoer WHERE opr_id = input_id;
 END //
 DELIMITER ;
