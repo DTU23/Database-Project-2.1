a64cee4244f5fd61b588d188bc3cebcf848600ca
Christian Niemann <spirem@gmail.com>
2017-04-01 16:26:36 +0200

    correction/translation/clean up

--- src/daoimpl01917/MySQLOperatorDAO.java
+++ src/daoimpl01917/MySQLOperatorDAO.java
@@ -14,27 +14,22 @@ import dto01917.OperatorDTO;
 public class MySQLOperatorDAO implements OperatorDAO {
 
 	public OperatorDTO getOperator(int oprId) throws DALException {
-		ResultSet rs = Connector.doQuery(
-
-				"SELECT * FROM operatoer WHERE opr_id = " + oprId);
+		ResultSet rs = Connector.doQuery("SELECT * FROM operatoer WHERE opr_id = " + oprId + ";");
 		try {
-			if (!rs.first()) throw new DALException("Operatoeren med id " + oprId + " findes ikke");
+			if (!rs.first()) throw new DALException("Operator with id " + oprId + " does not exist");
 			return new OperatorDTO (rs.getInt("opr_id"), rs.getString("opr_name"), rs.getString("ini"), rs.getString("cpr"), rs.getString("password"), rs.getBoolean("admin"), rs.getString("role"));
 		}
 		catch (SQLException e) {throw new DALException(e); }
-
 	}
-
-	// herunder har vi lavet
 
 	public void createOperator(OperatorDTO opr) throws DALException {		
 		Connector.doUpdate("CALL create_operator(" + opr.getOprId() + "," + opr.getOprName() + "," + opr.getIni() + "," + 
-				opr.getCpr() + "," + opr.getPassword() + ");");
+				opr.getCpr() + "," + opr.getPassword() + "," + opr.getAdmin() + "," + opr.getRole() + ");");
 	}
 
 	public void updateOperator(OperatorDTO opr) throws DALException {
 		Connector.doUpdate("CALL update_operator(" + opr.getOprId() + "," + opr.getOprName() + "," + opr.getIni() + "," + 
-				opr.getCpr() + "," + opr.getPassword() + ");");
+				opr.getCpr() + "," + opr.getPassword() + "," + opr.getAdmin() + "," + opr.getRole() + ");");
 	}
 
 	public List<OperatorDTO> getOperatorList() throws DALException {
@@ -51,10 +46,10 @@ public class MySQLOperatorDAO implements OperatorDAO {
 		return list;
 	}
 
-	public String getOperatorNameFromID(int id) throws DALException {
-		ResultSet rs = Connector.doQuery("CALL get_operator_name_from_id(" + id + ");");
+	public String getOperatorNameFromID(int oprId) throws DALException {
+		ResultSet rs = Connector.doQuery("CALL get_operator_name_from_id(" + oprId + ");");
 		try {
-			if (!rs.first()) throw new DALException("Operatoeren med id " + id + " findes ikke");
+			if (!rs.first()) throw new DALException("Operator with id " + oprId + " does not exist");
 			return rs.getString("opr_name");
 		}
 		catch (SQLException e) {throw new DALException(e); }
