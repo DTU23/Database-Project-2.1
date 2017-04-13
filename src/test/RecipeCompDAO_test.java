e40d3d43382a76604a35ffa14a75f3c57fdc989c
Unknown <vpmads@gmail.com>
2017-04-13 15:06:55 +0200

    Added positive tests, still missing some negative

--- src/test/RecipeCompDAO_test.java
+++ src/test/RecipeCompDAO_test.java
@@ -2,28 +2,113 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import daoimpl.MySQLRecipeDAO;
+import daoimpl.MySQLRecipeCompDAO;
+import daointerfaces.DALException;
+import dto.RecipeCompDTO;
+import dto.RecipeDTO;
 
 public class RecipeCompDAO_test {
 
-	@Before
-	public void setUp() throws Exception {
-		recipedao = new MySQLRecipeDAO();
-	}
 
-	@After
-	public void tearDown() throws Exception {
-	}
+	MySQLRecipeCompDAO recipeComp = new MySQLRecipeCompDAO();
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void positiveGetRecipeComp() {
+		try {
+			RecipeCompDTO expected = new RecipeCompDTO(1, 1, 10, 0.1);
+			RecipeCompDTO actual = recipeComp.getRecipeComp(1, 1);
+			assertEquals(expected, actual);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
 	}
-
+	@Test
+	public void getRecipeOutOfBounds() {
+		try {
+			RecipeCompDTO expected = new RecipeCompDTO(4, 1, 0, 0.1); // Rettes til fejlmeddelse så den bliver positiv?
+			RecipeCompDTO actual = recipeComp.getRecipeComp(4,1);
+			assertEquals(expected, actual);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+	@Test
+	public void getRecipeIntPlusOne() {
+		try {
+			RecipeCompDTO expected = new RecipeCompDTO(Integer.MAX_VALUE+1, 1, 0, 0.1 );
+			RecipeCompDTO actual = recipeComp.getRecipeComp(Integer.MAX_VALUE+1, 1);
+			assertEquals(expected, actual);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+		@Test
+		public void positiveGetListRecipeWithID() {
+			try {
+				List<RecipeCompDTO> expected = new ArrayList<RecipeCompDTO>();
+				expected.add(new RecipeCompDTO(1, 1, 10, 0.1));
+				expected.add(new RecipeCompDTO(1, 2, 2, 0.1));
+				expected.add(new RecipeCompDTO(1, 5, 2, 0.1));
+				List<RecipeCompDTO> actual = recipeComp.getRecipeCompList(1);
+				assertEquals(expected, actual);
+			} catch (DALException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		}
+	
+	@Test
+		public void positiveGetListRecipe() {
+			try {
+				List<RecipeCompDTO> expected = new ArrayList<RecipeCompDTO>();
+				expected.add(new RecipeCompDTO(1, 1, 10, 0.1));
+				expected.add(new RecipeCompDTO(1, 2, 2, 0.1));
+				expected.add(new RecipeCompDTO(1, 5, 2, 0.1));
+				expected.add(new RecipeCompDTO(2, 1, 10, 0.1));
+				expected.add(new RecipeCompDTO(2, 3, 2, 0.1));
+				expected.add(new RecipeCompDTO(2, 5, 1.5, 0.1));
+				expected.add(new RecipeCompDTO(2, 6, 1.5, 0.1));
+				expected.add(new RecipeCompDTO(3, 1, 10, 0.1));
+				expected.add(new RecipeCompDTO(3, 4, 1.5, 0.1));
+				expected.add(new RecipeCompDTO(3, 5, 1.5, 0.1));
+				expected.add(new RecipeCompDTO(3, 6, 1, 0.1));
+				expected.add(new RecipeCompDTO(3, 7, 1, 0.1));
+				List<RecipeCompDTO> actual = recipeComp.getRecipeCompList();
+				assertEquals(expected, actual);
+			} catch (DALException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	}
+	
+	@Test
+	public void positiveCreateRecipeComp() {
+		try {
+			RecipeCompDTO recipecomp1 = new RecipeCompDTO(4, 1, 1.5, 0.1);
+			recipeComp.createRecipeComp(recipecomp1);
+			RecipeCompDTO expected = new RecipeCompDTO(4, 1, 1.5, 0.1);
+			RecipeCompDTO actual = recipeComp.getRecipeComp(4, 1);
+			assertEquals(expected, actual);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
 }
--- src/test/RecipeDAO_test.java
+++ src/test/RecipeDAO_test.java
@@ -2,23 +2,84 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import daoimpl.MySQLRecipeDAO;
 import daointerfaces.DALException;
+import daointerfaces.ProductBatchCompDAO;
+import dto.RecipeDTO;
 
 public class RecipeDAO_test {
 
 	MySQLRecipeDAO recipe = new MySQLRecipeDAO();
 
 	@Test
-	public void test() {
+	public void positiveGetRecipe() {
 		try {
-			recipe.getRecipe(3);
-			
+			RecipeDTO expected = new RecipeDTO(3, "capricciosa");
+			RecipeDTO actual = recipe.getRecipe(3);
+			assertEquals(expected, actual);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+	@Test
+	public void getRecipeOutOfBounds() {
+		try {
+			RecipeDTO expected = new RecipeDTO(5, null); // Rettes til fejlmeddelse så den bliver positiv?
+			RecipeDTO actual = recipe.getRecipe(5);
+			assertEquals(expected, actual);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	}
+	@Test
+	public void getRecipeIntPlusOne() {
+		try {
+			RecipeDTO expected = new RecipeDTO(Integer.MAX_VALUE+1, null);
+			RecipeDTO actual = recipe.getRecipe(Integer.MAX_VALUE+1);
+			assertEquals(expected, actual);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+		@Test
+		public void positiveGetListRecipe() {
+			try {
+				List<RecipeDTO> expected = new ArrayList<RecipeDTO>();
+				expected.add(new RecipeDTO(1, "margherita"));
+				expected.add(new RecipeDTO(2, "prosciutto"));
+				expected.add(new RecipeDTO(3, "capricciosa"));
+				List<RecipeDTO> actual = recipe.getRecipeList();
+				assertEquals(expected, actual);
+			} catch (DALException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		}
+	
+	@Test
+	public void positiveCreateRecipe() {
+		try {
+			RecipeDTO recipe1 = new RecipeDTO(4, "salami");
+			recipe.createRecipe(recipe1);
+			RecipeDTO expected = new RecipeDTO(4, "salami");
+			RecipeDTO actual = recipe.getRecipe(4);
+			assertEquals(expected, actual);
 		} catch (DALException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
